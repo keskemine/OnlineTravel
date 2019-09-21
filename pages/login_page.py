9aa8e59c200be4ff9b85d757f0c2b5b97e0ec982
keskinemine <keskincanemine@gmail.com>
2019-09-21 14:47:20 -0400

    changed loginpart

--- pages/login_page.py
+++ pages/login_page.py
@@ -13,47 +13,116 @@ class LoginPage(SeleniumDriver):
 
     _account="//button[@id='header-account-menu']"
     _sign_in="//a[@id='account-signin']"
-    _email_address="#gss-signin-email"
-    _password="#gss-signin-password"
-    _signin="//button[@id='gss-signin-submit']"
-
-
+    _email_address="//input[@id='gss-signin-email']"
+    _password="//input[@id='gss-signin-password']"
+    _signin_btn="//button[@id='gss-signin-submit']"
+    _forgot_password="//a[@id='gss-go-to-forgot-password']"
+    _reset_password="//button[@id='gss-forgot-password-submit']"
 
 
 
     def clickAccount(self):
-        self.elementClick(self._account, locatorType='xpath')
+        self. elementClick(self._account, locatorType="xpath")
 
-    def clickSign_In(self):
-        self.elementClick(self._sign_in, locatorType='xpath')
+    def clickSign_in(self):
+        self.elementClick(self._sign_in,locatorType="xpath")
 
     def sendkeyEmail(self, email):
-        self.sendKeys(email, self._email_address, locatorType="css")
+        self.sendKeys(email, self._email_address, locatorType="xpath")
 
-    def sendkeyPassword(self, psswrd):
-        self.sendKeys(psswrd, self._password, locatorType="css")
+    def sendkeyPassword(self, password):
+        self.sendKeys(password, self._password,locatorType="xpath")
 
-    def clickSignIn(self):
-        self.elementClick(self._signin, locatorType='xpath')
+    def clickSignInBtn(self):
+        self.elementClick(self._signin_btn,locatorType="xpath")
+
+    def clickForgotPassword(self):
+        self.elementClick(self._forgot_password,locatorType="xpath")
+
+    def clickResetPassword(self):
+        self.elementClick(self._reset_password, locatorType="xpath")
 
 
 
-    def login(self, email="", psswrd=""):
-
+    def account(self):
         self.clickAccount()
-        time.sleep(1)
+        time.sleep(2)
 
-        self.clickSign_In()
-        time.sleep(1)
+    def signIn(self):
+        self.clickSign_in()
+        time.sleep(2)
 
+    def email(self,email):
         self.sendkeyEmail(email)
-        time.sleep(1)
+        time.sleep(2)
 
-        self.sendkeyPassword(psswrd)
-        time.sleep(1)
+    def forgotPassword(self):
+        self.clickForgotPassword()
+        time.sleep(2)
 
-        self.clickSignIn()
-        time.sleep(1)
+    def resetPassword(self):
+        self.clickResetPassword()
+        time.sleep(2)
+
+    def password(self, password):
+        self.sendkeyPassword(password)
+        time.sleep(2)
+
+    def signInBtn(self):
+        self.clickSignInBtn()
+        time.sleep(2)
+
+    def blankemail(self,email,password):
+        self.clickAccount()
+        time.sleep(2)
+        self.clickSign_in()
+        time.sleep(2)
+        self.sendkeyEmail(email)
+        time.sleep(2)
+        self.sendkeyPassword(password)
+        time.sleep(2)
+        self.clickSignInBtn()
+        time.sleep(2)
+
+
+    def blankPassword(self,email,password):
+        self.clickAccount()
+        time.sleep(2)
+        self.clickSign_in()
+        time.sleep(2)
+        self.sendkeyEmail(email)
+        time.sleep(2)
+        self.sendkeyPassword(password)
+        time.sleep(2)
+        self.clickSignInBtn()
+        time.sleep(2)
+
+
+    def invalidEmail(self,email, password):
+        self.clickAccount()
+        time.sleep(2)
+        self.clickSign_in()
+        time.sleep(2)
+        self.sendkeyEmail(email)
+        time.sleep(2)
+        self.sendkeyPassword(password)
+        time.sleep(2)
+        self.clickSignInBtn()
+        time.sleep(2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- tests/login_test.py
+++ tests/login_test.py
@@ -7,47 +7,105 @@ import allure
 import pytest
 import time
 
-class LoginTest(unittest.TestCase): # LOGIN PART
+class LoginTest(unittest.TestCase):# LOGIN PART
+    global webdriver
 
-    def test1(self): #getting into account
-        # wd = WebDriverFactory(browser="chrome")
-        # driver = wd.getWebDriverInstance()
-
-        name = driver.find_element_by_xpath("//button[@id='header-account-menu-signed-in']").text
-        if name=="Hello, summer":
-            print("TC001 is passed")
-        else:
-            print("TC001 is failed")
-
-
-        fp = LoginPage(driver)
-        fp.login("summer2019@gmail.com", "Smmr2019")
-
-    def test2(self): #wrong email address
-
-        # wd = WebDriverFactory(browser="firefox")
-        # driver = wd.getWebDriverInstance()
-
-        errormassege = driver.find_element_by_xpath("//div[@id='gss-signin-incorrect-email-or-password']")
-        if errormassege.is_displayed():
-            print("TC002 is passed")
-        else:
-            print("TC002 is failed")
-
-        fp = LoginPage(driver)
-        fp.login("summar2019@gmail.com", "Smmr2019")
-
-
-    def test3(self): #wrong password
-
-        wd = WebDriverFactory(browser="firefox")
+    def test_login(self):
+        wd = WebDriverFactory(browser= "chrome.driver")
         driver = wd.getWebDriverInstance()
+        lt = LoginPage(driver)
 
-        errormassege = driver.find_element_by_xpath("//div[@id='gss-signin-incorrect-email-or-password']")
-        if errormassege.is_displayed():
-            print("TC003 is passed")
+        # Click and verify account
+        lt.account()
+        element = driver.find_element_by_xpath("//button[@id='header-account-menu']")
+        if element.is_enabled():
+            print("Test case TC001: Pass")
         else:
-            print("TC003 is failed")
+            print("Test Case TC001: Fail")
 
-        fp = LoginPage(driver)
-        fp.login("summer2019@gmail.com", "Smmer2019")
+        # Click and verify Sign in
+        lt.signIn()
+        element = driver.find_element_by_xpath("//a[@id='account-signin']")
+        if element.is_enabled():
+            print("Test Case Tc002: Pass")
+        else:
+            print("Test Case TC002: Fail")
+
+        # Send email and verify email
+        lt.email("rosemily.johnson@gmail.com")
+        email = driver.find_element_by_xpath("//input[@id='gss-signin-email']")
+        if email.is_enabled():
+            print("Test Case Tc003: Pass")
+        else:
+            print("Test Case TC003: Fail")
+
+        # # Click and verify 'Forgot Password Button'
+        # lt.forgotPassword()
+        # heading = driver.find_element_by_xpath("//h2[@id='gss-forgot-password-heading']")
+        # print(heading.text)
+        # if heading.text=="Reset your password":
+        #     print("Test Case TC004: Pass")
+        # else:
+        #     print("Test Case TC004: Fail ")
+
+        # Send Password and Verify
+        lt.password("Ej12345679*")
+        password = driver.find_element_by_xpath("//input[@id='gss-signin-password']")
+        if password.is_enabled():
+            print("Test Case TC004: Pass")
+        else:
+            print("Test Case TC004: Fail")
+
+
+        # Click and verify Sign In Button
+        lt.signInBtn()
+        header_account = driver.find_element_by_xpath("//button[@id='header-account-menu-signed-in']")
+        if header_account.is_enabled():
+            print("Test Case TC005: Pass")
+        else:
+            print("Test Case TC005: Fail")
+
+    def test_blank_email(self):
+        wd = WebDriverFactory(browser="chrome.driver")
+        driver = wd.getWebDriverInstance()
+        lt = LoginPage(driver)
+
+        #Verify that the email field must be fill out
+        lt.blankemail("","Ej12345679*")
+        message = driver.find_element_by_xpath("//p[@id='signInEmailErrorMessage']")
+        print(message.text)
+        if message.text=="Please enter an email address":
+            print("Test Case TC006: Pass")
+        else:
+            print("Test Case TC006: Fail")
+
+
+    def test_blank_password(self):
+        wd = WebDriverFactory(browser="chrome.driver")
+        driver = wd.getWebDriverInstance()
+        lt= LoginPage(driver)
+
+        #Verify that the password field must be fill out
+        lt.blankPassword("rosemily.johnson@gmail.com","")
+        message = driver.find_element_by_xpath("//p[@id='signInPasswordErrorMessage']")
+        print(message.text)
+        if message.text=="Please enter a password":
+            print("Test Case TC007: Pass")
+        else:
+            print("Test Case TC007: Fail")
+
+
+    def test_validation(self):
+        wd= WebDriverFactory(browser="chrome.driver")
+        driver = wd.getWebDriverInstance()
+        lt = LoginPage(driver)
+
+        #Verify that password and email must be correct
+        lt.invalidEmail("rosemily.johnson@gmail.com","Ej12345679")
+        message = driver.find_element_by_xpath("//div[@id='gss-signin-incorrect-email-or-password']")
+        print(message.text)
+        if message.text=="You may have entered an unknown email address or an incorrect password":
+            print("Test Case TC008: Pass" )
+        else:
+            print("Test Case TC008: Fail")
+
